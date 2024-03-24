7df2dd7d6a479a1425c2bf5a56d285b92bdd40b5
Anthony ubuntu pc <anthonykinzelin@gmail.com>
2024-03-24 18:09:13 +0100

    saved

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: akinzeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 12:28:44 by akinzeli          #+#    #+#             */
-/*   Updated: 2024/03/24 17:43:32 by akinzeli         ###   ########.fr       */
+/*   Updated: 2024/03/24 18:08:33 by akinzeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,15 @@ int	main(int argc, char **argv)
 	sigemptyset(&action.sa_mask);
 	action.sa_flags = 0;
 	if (sigaction(SIGUSR1, &action, NULL) == -1)
+	{
 		ft_printf("Error sigaction\n");
+		exit(EXIT_FAILURE);
+	}
 	if (sigaction(SIGUSR2, &action, NULL) == -1)
+	{
 		ft_printf("Error sigaction\n");
+		exit(EXIT_FAILURE);
+	}
 	while (1)
 		pause();
 }
